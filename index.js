c04424f64b2587a46ae4ec76845bcd8999f56fa8
Md. Hasanur Rahman <hasanurhasu121@gmail.com>
2023-10-20 15:55:25 +0600

    added post

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -41,6 +41,13 @@ async function run() {
         app.get('/products', async(req, res) => {
             const cursor = brandShopCollections.find();
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id)};
+            const result = await brandShopCollections.findOne(query);
             res.send(result);
         })
 
